9571de9c87ffba9c4ee362cd2dcc39152f603cf9
zainul <helmi.zh28@gmail.com>
2023-05-01 17:54:33 +0530

    first commit

---
+++ hello.java
@@ -0,0 +1,13 @@
+public class hello{
+  public static void main(String [] args){
+ System.out.println("Hello Git !!");
+   System.out.println("Hello Java !!");
+   System.out.println("Hello Java !!");
+   System.out.println("Hello Java !!");
+   System.out.println("Hello Java !!");
+   System.out.println("Hello Java !!");
+   System.out.println("Hello Java !!");
+   System.out.println("Hello Java !!");
+   System.out.println("Hello Java !!");
+ }
+}
